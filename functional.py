39acc6235c3f71fd4939114299b0231d79808d8b
learningman <learningman7777@gmail.com>
2020-03-02 22:40:42 +0900

    feat: add softmax function

--- functional.py
+++ functional.py
@@ -1,5 +1,15 @@
 import numpy as np
 
 
-def relu(x):
-    return np.maximum(0, x)
+def relu(input):
+    return np.maximum(0, input)
+
+
+def softmax(input):
+    c = np.max(input)
+    exp_input = np.exp(input - c)
+    sum_exp_input = np.sum(exp_input)
+    ret = exp_input / sum_exp_input
+
+    return ret
+
